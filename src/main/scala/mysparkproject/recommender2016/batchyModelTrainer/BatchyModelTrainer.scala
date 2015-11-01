432310ee886289db8bd4481b13441284b5b68800
Yu Liu <yuliu@apache.org>
2015-10-31 23:27:24 -0700

    near working model trainer

--- src/main/scala/mysparkproject/recommender2016/batchyModelTrainer/BatchyModelTrainer.scala
+++ src/main/scala/mysparkproject/recommender2016/batchyModelTrainer/BatchyModelTrainer.scala
@@ -7,6 +7,8 @@ import org.apache.spark.mllib.recommendation.ALS
 import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
 import org.apache.spark.mllib.recommendation.Rating
 import scala.io.Source
+import java.nio.file.{Paths, Files}
+import java.io._
 
 object batchyModelTrainer {
   def main(args: Array[String]) {
@@ -23,57 +25,81 @@ object batchyModelTrainer {
     //parse acc ratings into tuple
     val ratingsTuples = ratingsLines.map(line => line.split(",") match {
       case Array(userid: String, trackid: String, timestamp: String, ratings: String) =>
-        (userid, trackid, timestamp.toLong, ratings)
+        (userid, trackid, timestamp.toLong, ratings.toDouble)
     })
 
     //filter ratings three years ago, tracks listened less than 10 times and not active within 6 months. visit external DB
     val ratingsWithinThreeYears = ratingsTuples.filter { rating => rating._3 > 0 } //mock 3 years here
       .filter(rating => rating._3 > 0) //mock 10 times and 6 months
-      .groupBy(rating => rating._1)
+      .groupBy(rating => rating._1)//should we do on scala level, but not RDD level(to prevent shuffle scheduling)?
     //ratingsWithinThreeYears in the form of Map(userid -> list of ratings)
 
-    //read in daily ratings for each user
-    /*
-     * is it possible to read one file once per partition. note don't get out of memory
-     * choice A: split users in the partition into 5 groups
-     * 						instead of reading the file once for each user, read once for each group
-     * 						file size may not be around average, may go to say 8GB. 
-     * 						we can choose diff algorithms according to diff file size
-     * 
-     * mapPartition. if RDD can't fill in memory, will surely have out of memory exception? maybe not, we are using iterators
-     */
-    def readDailyRatingFromUserId(userId: String) = {
-      //mock hashing
-      val partitionId = 1 + userId.toInt % 4
+    //build a map from user+track to rating in memory from acc ratings 
+    //we read records in daily rating files one by one. as we read one, calculate the event's score and update the map
+    //if user+track doesn't exist yet, insert; otherwise, add current event score to the existing score
+    val ratingsWithDailyRating = ratingsWithinThreeYears.mapPartitions( itr =>{
+      //maintain a map of userId+trackId -> (time,rating)
+      val accMap:scala.collection.mutable.Map[String, (Long,Double)] = scala.collection.mutable.Map()
+      var element = ("", Iterable[(String,String,Long,Double)]())
+      while(itr.hasNext){
+        element = itr.next
+        for(pair <- element._2){
+          //compose composite key: user id + track id
+          val key = pair._1 + "," + pair._2
+          accMap.put(key,(pair._3, pair._4))
+        }
+      }
+       //mock hashing. use the last usersId is ok
+      val partitionId = 1 + element._1.toInt % 4
       //mock file name. name should be fetched from config file and contain date
       val partitionFileName = "/Users/yliu/deployment/recommendationProject/daily_user_track_event_00" + partitionId + ".txt"
       // val partitionFileName = "/Users/yliu/deployment/recommendationProject/daily_user_track_event_001.txt"
       println("trying to read daily event partition file at: " + partitionFileName)
+      val randomGenerator = scala.util.Random
       var events = List[(String, String, String, String, String, String)]()
-      for (line <- Source.fromFile(partitionFileName).getLines()) {
-        val event = line.split(",") match {
-          case Array(userid: String, trackid: String, timestamp: String, msPlayed: String, reasonStart: String, reasonEnd: String) =>
-            (userid, trackid, timestamp, msPlayed, reasonStart, reasonEnd)
-        }
-        if (event._1 == userId) {
-          events ::= event
+      if(Files.exists(Paths.get(partitionFileName))){
+        for (line <- Source.fromFile(partitionFileName).getLines()) {
+          val event = line.split(",") match {
+            case Array(userid: String, trackid: String, timestamp: String, msPlayed: String, reasonStart: String, reasonEnd: String) =>
+              (userid, trackid, timestamp.toLong, msPlayed, reasonStart, reasonEnd)
+          }
+          //mock calculating score
+          val thisEventScore = randomGenerator.nextDouble() * 5
+          //neglect filter tracks listened less than 10 times or not active within 6 months
+          //insert into map
+          val key = event._1+","+event._2
+          val previousScoreAndTime = accMap.getOrElse(key, (0,0D))
+          accMap.put(key, (event._3, previousScoreAndTime._2 + thisEventScore))
         }
       }
-      events
+      accMap.iterator
     }
-    val ratingsWithDailyRating = ratingsWithinThreeYears.map(rating => rating._1 -> (rating._2, readDailyRatingFromUserId(rating._1)))
+    , true)
+    
+    
     //in the form of userid -> (list of acc ratings, list of daily events)
-    ratingsWithDailyRating.foreach { case (k, v) => println("key: " + k + " value: " + v.toString()) }
+    ratingsWithDailyRating.map { case (k, v) => println("key: " + k + " value: " + v.toString()) }
 
     //filter tracks listened less than 10 times or not active within 6 months. visit external DB
     val activeRatings = ratingsWithDailyRating.filter(rating => rating._1 != "") //mock 10 & 6 here
 
-    //mock merge ratings by user id & track id and score calculation process
-    def convertToRating(userId: Int, trackId: Int) = {
-      val randomGenerator = scala.util.Random //need to be put to partition level
-      Rating(userId.toInt, trackId.toInt, randomGenerator.nextDouble() * 5)
+    //convert rating map entry into Rating objects
+    def convertToRating(rating: (String, (Long, Double))) = {
+      val userId = rating._1.split(",")(0)
+      val trackId = rating._1.split(",")(1)
+      Rating(userId.toInt, trackId.toInt, rating._2._2)
     }
-    val eventRatings = ratingsTuples.map(event => convertToRating(event._1.toInt, event._2.toInt))
+    
+    //write back to acc rating files. for fault tolerant purpose, we write to temp files and then rename
+    activeRatings.mapPartitions( rating => {
+      val writer = new PrintWriter(new File("accumulatedRatings-new-001-001.txt" ))
+      activeRatings.map(rating => writer.write(rating._1 + "," + rating._2._1 + "," + rating._2._2))
+      writer.close()
+      "not need for return".iterator
+    }
+    , true)
+    
+    val eventRatings = activeRatings.map(rating => convertToRating(rating))
 
     //train the model with ALS
     //those will be read from config files
