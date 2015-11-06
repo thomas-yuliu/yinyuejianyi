a22fb9c778b52e1d96264b9789771e6866c7bd1d
Yu Liu <yuliu@apache.org>
2015-11-06 12:05:26 -0800

    generate random commit

--- generateDailyEvents.sh
+++ generateDailyEvents.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
+#what this script will do is:
+# generate a stub file
+# duplicate its content and save to itself
+# keep doubling size until reach the limit
 
 filename=daily_user_track_event.txt;
-filesize=$(wc -c <"$filename");
-filelimit=2000000000;
+finalfilelimit=2000000000;
 
 rm $filename;
 touch $filename;
 
-while [ $filesize -le $filelimit ];
+stubfilelimit=100000;#100KB
+filesize=$(wc -c <"$filename");
+while [ $filesize -le $stubfilelimit ];
 do
 	userId=$RANDOM;
-	$(($userId%=720));
+	((userId=$userId%720));
 	trackId=$RANDOM;
-	$(($trackId%=240));
+	((trackId=$trackId%240));
 	timestamp=1445047130;
 	percentage=0.6;
 	eventSource="browsing";
 	nextEvent="null";
 	
 	echo $userId , $trackId , $timestamp , $percentage, $eventSource , $nextEvent >> $filename;
+	
+	filesize=$(wc -c <"$filename");
+done;
+
+interfilename=inter.txt
+while [ $filesize -le $finalfilelimit ];
+do
+	#override interfilename
+	echo "" > $interfilename
+	
+	#each time double size
+	cat $filename >> $interfilename
+	cat $interfilename >> $filename
 	
 	filesize=$(wc -c <"$filename");
 done;
