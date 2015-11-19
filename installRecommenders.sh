cb0962c7e987ee99772e0001a772f1f4325431f2
Yu Liu <yuliu@apache.org>
2015-11-18 19:30:54 -0800

    fixing deployment script bugs

--- installRecommenders.sh
+++ installRecommenders.sh
@@ -12,22 +12,23 @@ gcloud compute copy-files ~/mavenWorkspace/spark-project/musicRecommender/config
 # run from your desktop
 
 
-# run from the VM
 sudo su -
 yum install java-1.7.0-openjdk -y
 export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk.x86_64/
 export INSTALL_LOCATION=/sparkproject
 yum install wget -y
 mkdir $INSTALL_LOCATION
+mkdir ($INSTALL_LOCATION)/sparkeventlogs
 cd $INSTALL_LOCATION
 wget http://d3kbcqa49mib13.cloudfront.net/spark-1.5.1-bin-hadoop2.6.tgz
 tar zxvf spark-1.5.1-bin-hadoop2.6.tgz
 
 cd spark-1.5.1-bin-hadoop2.6
-export SPARK_SSH_FOREGROUND=vagrant
 
 
+# run from the VM
 # standalone cluster batch rec
+cd spark-1.5.1-bin-hadoop2.6
 bin/spark-submit --class mysparkproject.recommender2016.batchyRecommender.BatchRecommender --master spark://c6501.ambari.apache.org:7077  --executor-memory 1G --total-executor-cores 3 ../musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
 # standalone cluster model trainer
@@ -64,17 +65,26 @@ bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
 
 
 # on local vagrant VM only
+# first on workspace
 cp ~/mavenWorkspace/spark-project/musicRecommender/target/musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/ambari-vagrant/centos6.5
-cp config/recommenderConfig.json  ~/ambari-vagrant/centos6.5
+cp ~/mavenWorkspace/spark-project/musicRecommender/config/recommenderConfig.json  ~/ambari-vagrant/centos6.5
 # then on vm 
 sudo su -
 export INSTALL_LOCATION=/sparkproject
 cd $INSTALL_LOCATION
 
 cp /vagrant/musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar .
+mkdir config
 cp /vagrant/recommenderConfig.json config
 cp /vagrant/acc* .
 cp /vagrant/daily* .
+cp /vagrant/spark-defaults.conf /sparkproject/spark-1.5.1-bin-hadoop2.6/conf
+
+#on master only
+export INSTALL_LOCATION=/sparkproject
+cp /vagrant/slaves $INSTALL_LOCATION/spark-1.5.1-bin-hadoop2.6/conf
+sbin/start-all.sh
+#manually enter password 'vagrant' for all hosts for now
 
 # vm make space
 rm -rf /sparkproject/spark-1.5.1-bin-hadoop2.6/work/*
