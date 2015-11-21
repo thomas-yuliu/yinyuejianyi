b7d3c1d77a3f3aa48bed1982ff99679e58f2f4b5
Yu Liu <yuliu@apache.org>
2015-11-21 12:55:26 -0800

    improving install script

--- installRecommenders.sh
+++ installRecommenders.sh
@@ -23,19 +23,20 @@ cd $INSTALL_LOCATION
 wget http://d3kbcqa49mib13.cloudfront.net/spark-1.5.1-bin-hadoop2.6.tgz
 tar zxvf spark-1.5.1-bin-hadoop2.6.tgz
 
-cd spark-1.5.1-bin-hadoop2.6
 
 
 # run from the VM
 # standalone cluster batch rec
-cd spark-1.5.1-bin-hadoop2.6
-bin/spark-submit --class mysparkproject.recommender2016.batchyRecommender.BatchRecommender --master spark://c6501.ambari.apache.org:7077  --executor-memory 1G --total-executor-cores 3 ../musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+cd $INSTALL_LOCATION/spark-1.5.1-bin-hadoop2.6
+bin/spark-submit --class mysparkproject.recommender2016.batchyRecommender.BatchRecommender --master spark://c6401.ambari.apache.org:7077  --executor-memory 5G --total-executor-cores 3 ../musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
 # standalone cluster model trainer
-bin/spark-submit --class mysparkproject.recommender2016.batchyModelTrainer.batchyModelTrainer --master spark://c6501.ambari.apache.org:7077  --executor-memory 1G --total-executor-cores 3 ../musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+cd $INSTALL_LOCATION/spark-1.5.1-bin-hadoop2.6
+bin/spark-submit --class mysparkproject.recommender2016.batchyModelTrainer.batchyModelTrainer --master spark://c6401.ambari.apache.org:7077  --executor-memory 5G --total-executor-cores 3 ../musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
 # standalone streaming rec
-bin/spark-submit --class mysparkproject.recommender2016.streamingRecommender.StreamingRecommender --master spark://c6501.ambari.apache.org:7077 --executor-memory 1G --total-executor-cores 3 ../musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+cd $INSTALL_LOCATION/spark-1.5.1-bin-hadoop2.6
+bin/spark-submit --class mysparkproject.recommender2016.streamingRecommender.StreamingRecommender --master spark://c6401.ambari.apache.org:7077 --executor-memory 5G --total-executor-cores 3 ../musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
 
 
@@ -68,34 +69,41 @@ bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
 # first on workspace
 cp ~/mavenWorkspace/spark-project/musicRecommender/target/musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar ~/ambari-vagrant/centos6.4
 cp ~/mavenWorkspace/spark-project/musicRecommender/config/recommenderConfig.json  ~/ambari-vagrant/centos6.4
-cp ~/mavenWorkspace/spark-project/musicRecommender/installRecommenders.sh  ~/ambari-vagrant/centos6.4
+cp ~/mavenWorkspace/spark-project/musicRecommender/generateDailyEvents.sh  ~/ambari-vagrant/centos6.4
+cp ~/deployment/spark-1.3.1-bin-hadoop2.6/conf/slaves ~/ambari-vagrant/centos6.4/
+cp ~/deployment/spark-1.3.1-bin-hadoop2.6/conf/log4j.properties ~/ambari-vagrant/centos6.4/
+cp ~/deployment/spark-1.3.1-bin-hadoop2.6/conf/spark-defaults.conf ~/ambari-vagrant/centos6.4/
+cp ~/deployment/spark-1.3.1-bin-hadoop2.6/conf/spark-env.sh ~/ambari-vagrant/centos6.4/
+
+
 # then on vm 
 sudo su -
 export INSTALL_LOCATION=/sparkproject
 cd $INSTALL_LOCATION
-
-cp /vagrant/musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar .
-cp /vagrant/installRecommenders.sh .
+yes | cp /vagrant/musicRecommender-0.0.1-SNAPSHOT-jar-with-dependencies.jar .
+yes | cp /vagrant/generateDailyEvents.sh .
+./generateDailyEvents.sh
 mkdir config
-cp /vagrant/recommenderConfig.json config
+yes | cp /vagrant/recommenderConfig.json config
+yes | cp /vagrant/spark-defaults.conf /sparkproject/spark-1.5.1-bin-hadoop2.6/conf
+yes | cp /vagrant/spark-env.sh /sparkproject/spark-1.5.1-bin-hadoop2.6/conf
+yes | cp /vagrant/log4j.properties /sparkproject/spark-1.5.1-bin-hadoop2.6/conf
+yes | cp /vagrant/slaves /sparkproject/spark-1.5.1-bin-hadoop2.6/conf
 #cp /vagrant/acc* .
 #cp /vagrant/daily* .
 mkdir $INSTALL_LOCATION/sparkeventlogs
-cp /vagrant/spark-defaults.conf /sparkproject/spark-1.5.1-bin-hadoop2.6/conf
-cp /vagrant/spark-env.sh /sparkproject/spark-1.5.1-bin-hadoop2.6/conf
-cp /vagrant/log4j.properties /sparkproject/spark-1.5.1-bin-hadoop2.6/conf
-cp /vagrant/slaves /sparkproject/spark-1.5.1-bin-hadoop2.6/conf
+export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk.x86_64/
 
 #on master only
 export INSTALL_LOCATION=/sparkproject
-cp /vagrant/slaves $INSTALL_LOCATION/spark-1.5.1-bin-hadoop2.6/conf
+cd $INSTALL_LOCATION/spark-1.5.1-bin-hadoop2.6
 sbin/start-all.sh
 #manually enter password 'vagrant' for all hosts for now
 
 # vm make space
 rm -rf /sparkproject/spark-1.5.1-bin-hadoop2.6/work/*
 
-#clean up
+#clean up input files
 rm -f accumulatedRatings-new*
 
 
